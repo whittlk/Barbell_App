c31d0a2ddd190bf756bcc10c9b8ed5ba3f181699
Korbin Dansie <dan15013@byui.edu>
2019-10-29 13:47:03 -0600

    Korbin Dansie Push Test

--- app/src/main/java/com/example/barbellapp/MainActivity.java
+++ app/src/main/java/com/example/barbellapp/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
-/* Test Change */
+/* Test Korbin */
 /* Korbin Dansie*/
 /* hello dolly*/
