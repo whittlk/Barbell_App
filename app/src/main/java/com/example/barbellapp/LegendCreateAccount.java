fbda3086e77927bc17acaf3ce3a35113c715cfb1
Korbin-Dansie <Dansiekm@gmail.com>
2019-11-26 12:31:41 -0700

    Fixed password not comparing

--- app/src/main/java/com/example/barbellapp/LegendCreateAccount.java
+++ app/src/main/java/com/example/barbellapp/LegendCreateAccount.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,6 +20,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class LegendCreateAccount extends AppCompatActivity {
 
+    private static final String TAG = "LegendCreateAccount";
     EditText fName;
     EditText lName;
     EditText email;
@@ -29,8 +31,6 @@ public class LegendCreateAccount extends AppCompatActivity {
     String p;
     String cP;
     boolean matches;
-    private static final String TAG = "LegendCreateAccount";
-
     FirebaseAuth firebaseAuth;
 
     @Override
@@ -42,25 +42,27 @@ public class LegendCreateAccount extends AppCompatActivity {
         fName = findViewById(R.id.first_name_input);
         lName = findViewById(R.id.last_name_input);
         email = findViewById(R.id.r_email_address_input);
-        pass = findViewById(R.id.r_password_input);
-        confirmPass = findViewById(R.id.re_enter_password_input);
+        final EditText pass = findViewById(R.id.r_password_input);
+        final EditText confirmPass = findViewById(R.id.re_enter_password_input);
         register = findViewById(R.id.submit);
         progressBar = findViewById(R.id.r_progressBar);
-        p = pass.getText().toString();
-        cP = confirmPass.getText().toString();
+        String p = pass.getText().toString();
+        String cP = confirmPass.getText().toString();
 
 
-        firebaseAuth = firebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
 
         //TODO
-        register.setOnClickListener(new View.OnClickListener(){
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String password = pass.getText().toString();
+                String Cpassword = confirmPass.getText().toString();
                 //Create a string for P and CP
                 // P: password
                 // cp: Compare password
-                if (!p.equals(cP)) {
-                    Toast.makeText(LegendCreateAccount.this, "Passwords Do Not Match",
+                if (!password.equals(Cpassword)) {
+                    Toast.makeText(getApplicationContext(), "Passwords Do Not Match",
                             Toast.LENGTH_LONG).show();
                 }
                 //Register user in fire base
@@ -84,10 +86,9 @@ public class LegendCreateAccount extends AppCompatActivity {
                                         Log.e("SignupActivity", "Sign Up Failed");
                                     }
                                 }
-
                             });
                 }
             }
         });
-    }
+    }//End of on create
 }
