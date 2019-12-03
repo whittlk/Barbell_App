e4c824f0659ea35440863eb3f4be0830be9d9334
erinalicia02 <erinalicia02@users.noreply.github.com>
2019-12-03 13:33:08 -0800

    Erin Added comments and all my other stuff that I've been doing.
    Merge branch 'TestUI' of https://github.com/whittlk/Barbell_App
    
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/example/barbellapp/LegendsAddStrength.java
@@ -0,0 +1,14 @@
+package com.example.barbellapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LegendsAddStrength extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_legends_add_strength);
+    }
+}
---
+++ app/src/main/java/com/example/barbellapp/LegendsBootcampWODResults.java
@@ -0,0 +1,14 @@
+package com.example.barbellapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LegendsBootcampWODResults extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_legends_bootcamp_wodresults);
+    }
+}
