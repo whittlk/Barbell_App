b95a77f59033375bd1a3a49898c79d156d5885cf
erinalicia02 <erinalicia02@users.noreply.github.com>
2019-12-20 21:40:55 -0800

    Merge branch 'master' of https://github.com/whittlk/Barbell_App
    # Conflicts:
    #	app/src/main/res/layout/activity_legends_home.xml

---
+++ app/src/main/java/com/example/barbellapp/log_lift_back_squat.java
@@ -0,0 +1,14 @@
+package com.example.barbellapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class log_lift_back_squat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_lift_back_squat);
+    }
+}
